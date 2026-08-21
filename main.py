820d7b1044faa8f668b217301f196721eb377284
Ryo <m1tzy7194@gmail.com>
2026-08-21 12:04:54 -0300

    Exorcismo 4 Parte 3 (de cabeça pra baixo pra cima pra esquerda pra direita)

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def CalcularValorFinal(valor_base = 0.0, taxa_desconto = 0.0):
     valor_com_desconto = valor_base - (valor_base * (taxa_desconto / 100))
     return valor_com_desconto
 
-def ExibirRecibo(nome_produto = '', valor_original = 0.0, valor_pago = 0.0):
+def ExibirRecibo(nome_produto = '', valor_pago = 0.0):
     print(f'{nome_produto} - {valor_pago:.2f}')
 
 while True:
@@ -25,5 +25,5 @@ while True:
 
     if opcao.lower() != 's':
         for produto in produtos:
-            ExibirRecibo(produto[0], produto[1], produto[3])
+            ExibirRecibo(produto[0], produto[3])
         break
